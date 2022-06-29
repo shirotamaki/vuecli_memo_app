d4d6b648553b1d24e9c917cbd49d175abb169092
shirotamaki <shiro.tamaki63@gmail.com>
2022-06-29 17:28:24 +0900

    Modify: +ボタンorメモリストをクリックすると入力フォームが出現

--- vuecli_memo_app/src/components/MemoList.vue
+++ vuecli_memo_app/src/components/MemoList.vue
@@ -2,22 +2,25 @@
   <div>
     <ul>
       <li v-for="(memo, index) in memoList" :key="index">
-        <span @click="edit(index, memo)">{{ showTitle(memo) }}</span>
+        <span @click="edit(index, memo)">{{ displayTitle(memo) }}</span>
       </li>
     </ul>
   </div>
-  <div>
-    <form @submit.prevent>
-      <textarea type="text" v-model="text" ref="editor"></textarea>
-      <div>
-        <span><button class="button" @click="setText" type="submit">{{ '＋' }}</button></span>
-<!--        <span><button class="button" @click="cancel" type="submit">キャンセル</button></span>-->
-<!--        <span><button class="button" @click="cancel" type="submit">キャンセル</button></span>-->
-        <span><button class="button" @click="remove" type="submit">削除</button></span>
-      </div>
-    </form>
-  </div>
+
+  <form @submit.prevent>
+    <span><button class="button" @click="displayForm" type="submit">+</button></span>
+
+    <div class="container-form" v-if="inputMode">
+      <textarea class="textarea" type="text" v-model="text" ref="editor" required></textarea>
+    </div>
+
+    <div class="form_button">
+      <span><button class="button" @click="addMemo" type="submit">編集</button></span>
+      <span><button class="button" @click="deleteMemo" type="submit">削除</button></span>
+    </div>
+  </form>
 </template>
+
 <script>
 export default {
   props: ['memoList'],
@@ -27,32 +30,55 @@ export default {
       text: '',
       editIndex: -1,
       memos: [],
+      inputMode: false,
     }
   },
+
   methods: {
-    showTitle (memo) {
+    displayTitle (memo) {
       return memo.split(/\n/)[0]
     },
+    displayForm() {
+      this.inputMode = true
+    },
     edit (index, memo) {
+      this.inputMode = true
       this.editIndex = index
       this.text = memo
       this.$refs.editor.focus()
     },
-    setText () {
+    addMemo () {
       this.$emit('add', this.text, this.editIndex)
       this.cancel()
     },
     cancel () {
       this.text = ''
       this.editIndex = -1
+      this.inputMode = false
     },
-    remove () {
+    deleteMemo () {
       this.$emit('delete', this.editIndex)
-      this.cancel ()
+      this.cancel()
     },
   },
 }
 </script>
 
 <style>
+.container-form {
+  width: 200px;
+
+}
+.textarea {
+  width: 200px;
+  height: 200px;
+}
+
+.form_button {
+  /*text-align: center;*/
+}
+
+.button {
+  margin: 8px;
+}
 </style>
