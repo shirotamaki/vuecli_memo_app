aca2f54f76cd996059b29d54920a13fc284627cb
shirotamaki <shiro.tamaki63@gmail.com>
2022-07-10 23:05:50 +0900

    Add: +ボタンのコンポーネントを追加

---
+++ vuecli_memo_app/src/components/ButtonForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form @submit.prevent>
+    <span><button class="button" @click="openForm" type="submit">+</button></span>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    openForm () {
+      this.$emit('open')
+    },
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  margin: 8px 0px 8px 8px;
+}
+</style>
